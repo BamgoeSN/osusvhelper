f3686f45ce287d45cd8bd2c86f29f03f53f35ce6
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2021-01-25 10:15:42 +0900

    Update api.py
    Added apis for inserting and deleting elements

--- api.py
+++ api.py
@@ -4,3 +4,25 @@ data = 0
 
 def initialize(startTime, initialBPM, intervalLength):
     data = Data(startTime, initialBPM, intervalLength)
+
+def pickPalette(palette):
+    if palette == "sv":
+        return data.sv
+    elif palette == "vo":
+        return data.vo
+    else:
+        raise ValueError("Invalid palette type")
+
+def insertPoint(palette, pos, value):
+    target = pickPalette(palette)
+    point = Point(pos, value)
+    target.insert(point)
+
+def insertInterval(palette, startPos, startValue, endPos, endValue, easeType, io):
+    target = pickPalette(palette)
+    interval = Interval(startPos, startValue, endPos, endValue, easeType, io)
+    target.insert(interval)
+
+def deleteElement(palette, index):
+    target = pickPalette(palette)
+    target.delete(index)
